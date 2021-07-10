bc32e00cf075b04853d7cce62a7c5ecb7d865eb2
Jonathan Mendoza <patts2810@gmail.com>
2021-07-09 23:01:15 -0500

    create reducer and add provider

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,4 +1,26 @@
+import { types } from "../components/types/types";
 
 export const authReducer = ( state = {}, action ) => {
+
+  // {
+  //   uid: 'kjnkfuf3fh3fhhi',
+  //   name: 'Patts'
+
+  // }
+
+  switch (action.type) {
+    case types.login:
+      
+    return {
+      uid: action.payload.uid,
+      name: action.payload.displayName
+    }
+    
+    case types.logout:
+      
+    return { }
   
+    default:
+      return state;
+  }
 }
